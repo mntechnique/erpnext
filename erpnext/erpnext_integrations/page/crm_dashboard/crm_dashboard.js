976b8170b63e1681874f1062137b93deff9491d1
Saurabh <bhosalesaurabh8@gmail.com>
2018-04-05 17:42:07 +0530

    Added CRM Dashboard page
    (cherry picked from commit 90bfa9ee7f300cf60f39d302d389b624e1deb5dc)

--- erpnext/erpnext_integrations/page/crm_dashboard/crm_dashboard.js
+++ erpnext/erpnext_integrations/page/crm_dashboard/crm_dashboard.js
@@ -129,7 +129,6 @@ frappe.CallCenterConsole = Class.extend({
 						console.log("R",r.message)
 						content = frappe.render_template("telephony_console", {"info": r.message || null});
 						me.page.main.append(content);
-						
 						if (r.message.title == "Lead") {
 							me.page.main.find("#lead_to_customer").on("click", function() {
 								me.create_customer(r.message);
@@ -151,7 +150,7 @@ frappe.CallCenterConsole = Class.extend({
 							me.page.main.find("#new_caller_issue").on("click", function() {
 								me.create_issue(r.message);
 							});						
-						}							
+						}
 					}
 				}
 			});
