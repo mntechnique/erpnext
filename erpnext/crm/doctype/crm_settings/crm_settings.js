05e596857a85fc263be19685e036533fc4bffff2
Saurabh <bhosalesaurabh8@gmail.com>
2018-05-02 15:49:10 +0530

    Testing backdrop and popup restrictions

--- erpnext/crm/doctype/crm_settings/crm_settings.js
+++ erpnext/crm/doctype/crm_settings/crm_settings.js
@@ -3,12 +3,24 @@
 
 frappe.ui.form.on('CRM Settings', {
 	refresh: function(frm) {
-		// frappe.realtime.on('show_popup', (popup_data) => {
-		// 	frappe.msgprint({
-		// 		title: __('Incoming Call'),
-		// 		message: __(popup_data)
-		// 	});
-			
-		// });
+
 	}
 });
+
+// $(document).on('app_ready', function() {
+// 	frappe.realtime.on('show_popup', (popup_data) => {
+// 		frappe.msgprint({
+// 			title: __('Incoming Call'),
+// 			message: __(popup_data)
+// 		});
+// 	});
+// 	$('.modal-backdrop').unbind('click'); 
+// });
+
+// $(document).on('show_popup', (popup_data) => {
+// 		frappe.msgprint({
+// 			title: __('Incoming Call'),
+// 			message: __(popup_data)
+// 		});
+// 	});
+// };
